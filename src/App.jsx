050d8710f717cbd2330aa2a6fba4a23b78cf5e07
liaqat <mliaqat@ngnintl.com>
2024-02-07 16:53:40 +0300

    Add Cart Provider

--- src/App.jsx
+++ src/App.jsx
@@ -5,77 +5,11 @@ import { DUMMY_PRODUCTS } from "./dummy-products.js";
 import { ShoppingCart } from "./store/Shopping-Cart-Context.jsx";
 
 function App() {
-  const [shoppingCart, setShoppingCart] = useState({
-    items: [],
-  });
-
-  function handleAddItemToCart(id) {
-    setShoppingCart((prevShoppingCart) => {
-      const updatedItems = [...prevShoppingCart.items];
-
-      const existingCartItemIndex = updatedItems.findIndex(
-        (cartItem) => cartItem.id === id
-      );
-      const existingCartItem = updatedItems[existingCartItemIndex];
-
-      if (existingCartItem) {
-        const updatedItem = {
-          ...existingCartItem,
-          quantity: existingCartItem.quantity + 1,
-        };
-        updatedItems[existingCartItemIndex] = updatedItem;
-      } else {
-        const product = DUMMY_PRODUCTS.find((product) => product.id === id);
-        updatedItems.push({
-          id: id,
-          name: product.title,
-          price: product.price,
-          quantity: 1,
-        });
-      }
-
-      return {
-        items: updatedItems,
-      };
-    });
-  }
-
-  function handleUpdateCartItemQuantity(productId, amount) {
-    setShoppingCart((prevShoppingCart) => {
-      const updatedItems = [...prevShoppingCart.items];
-      const updatedItemIndex = updatedItems.findIndex(
-        (item) => item.id === productId
-      );
-
-      const updatedItem = {
-        ...updatedItems[updatedItemIndex],
-      };
-
-      updatedItem.quantity += amount;
-
-      if (updatedItem.quantity <= 0) {
-        updatedItems.splice(updatedItemIndex, 1);
-      } else {
-        updatedItems[updatedItemIndex] = updatedItem;
-      }
-
-      return {
-        items: updatedItems,
-      };
-    });
-  }
-
-  const cartCtx = {
-    items: shoppingCart.items,
-    addItemToCart: handleAddItemToCart,
-    updatedItemQty:handleUpdateCartItemQuantity
-  };
-
   return (
-    <ShoppingCart.Provider value={cartCtx}>
+    <ShoppingCartProvider>
       <Header />
       <Shop />
-    </ShoppingCart.Provider>
+    </ShoppingCartProvider>
   );
 }
 
